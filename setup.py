5916a82212f8fb0e30fb3aa476852cdb548fb1c0
Dominik Schöni <dominik@dominikschoeni.ch>
2015-07-06 17:22:28 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ setup(
     name="drf-mongo-filters",
     packages=["drf_mongo_filters",],
     version="1.1",
-    install_requires=["Django == 1.7",
-                      "mongoengine >= 0.8, < 0.9",
-                      "djangorestframework >= 3.0, < 3.1"],
+    install_requires=["Django == 1.8",
+                      "mongoengine >= 0.10",
+                      "djangorestframework >= 3.0"],
     # metadata for upload to PyPI
     author="Maxim Vasiliev",
     author_email="qwiglydee@gmail.com",
     description="Filtering support for Django Rest Framework Mongoengine.",
     keywords=["mongoengine", "django rest framework", "filtering"],
-    url="https://github.com/qwiglydee/drf-mongo-filters",
-    download_url="https://github.com/qwiglydee/drf-mongo-filters/releases",
+    url="https://github.com/dschoeni/drf-mongo-filters",
+    download_url="https://github.com/dschoeni/drf-mongo-filters/releases",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 2.7 :: Only",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ]
